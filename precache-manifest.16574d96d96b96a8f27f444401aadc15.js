36ceae98e0dc0c1f5953d0eb8db95f7d536996f0
Naveen Saggam <thenaveensaggam@gmail.com>
2019-09-06 19:07:36 +0530

    my first react app

---
+++ precache-manifest.16574d96d96b96a8f27f444401aadc15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602e13a07788364a49b86ec8e38f3c1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3356a28fd92e5bbe3cc",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "1873d012ceb1f6a3b7b7",
+    "url": "/static/js/2.5bc0451a.chunk.js"
+  },
+  {
+    "revision": "f3356a28fd92e5bbe3cc",
+    "url": "/static/js/main.03cea6b4.chunk.js"
+  },
+  {
+    "revision": "474f79572f729dca25cd",
+    "url": "/static/js/runtime~main.b56cd54a.js"
+  }
+]);
